ecbdf39c37c5eec0464e90248a9c3cbd49e3315b
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-06-19 13:03:33 -0400

    Adding DOM-load messages

--- app/assets/javascripts/paintr.js
+++ app/assets/javascripts/paintr.js
@@ -3,10 +3,10 @@
 
 //Bind your events here
 $(document).ready(function(){
-
+  console.log('The DOM is loaded!');
 });
 
 //Alternate (pretty) way to run code after DOM load
 // $(function(){
-
+//   console.log('The DOM is loaded (v2)!');
 // });
